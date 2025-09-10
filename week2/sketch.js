68ca2f3b0891e2cebcffbfd26bcabf85f965d2b6
bl4309 <bryanchihunlai@gmail.com>
2025-09-10 16:51:27 -0400

    first sketch.js play around

--- week2/sketch.js
+++ week2/sketch.js
@@ -1,10 +1,13 @@
 
 
-function setup() {
+function setup() { //runs once at start
   createCanvas(windowWidth, windowHeight);
-  
+  //createCanvas(x,y) is a function from p5.js reference
 }
 
-function draw() {
-  
+function draw() { //runs in loop after setup
+  circle(100,200,90); //draw a circle
+  //in p5.js, (0,0) is top left corner
+  //x is horizontal position, y is vertical position
+  //d is diameter of circle
 }
