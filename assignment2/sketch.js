d75c01f1d64a65b412e4c8166b6db80b1dee9880
bl4309 <bryanchihunlai@gmail.com>
2025-10-01 12:54:47 -0400

    assignment 2

--- assignment2/sketch.js
+++ assignment2/sketch.js
@@ -1,43 +1,40 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  // iteration operators:
-  // i++ : adds 1 to i
-  // i+=10 : adds 10 to i
-  // i-- : subtracts 1 from i
-  // i -=5 : subtracts 5 from i
-
-  for (let i = 0; i < 10; i++) {
-    console.log(i);
-  }
+//in all honesty this was a little rough for me, i understand the concepts but it got confusing as i progressed since i am kind of working backwards with your template
+//will show up to office hours next week
 }
-
+let r = 100;
 function draw() {
-  background("#eec869ff");
-  //noLoop(); // prevents draw from looping
-  for (let y = 50; y < height - 50; y += 100) {
-    for (let x = 50; x < width - 50; x += 100) {
+  background(mouseX, mouseY/2, 50, 50); //background color changes with mouse position but Y needed to be tamed
+
+  for (let y = 50; y < height - 50; y += 200) {
+    for (let x = 50; x < width - 50; x += 200) {
+
+
       push();
       translate(x, y);
       let rotation;
-      rotation = map(y,50,height-50,0,PI);
-      rotate(rotation);
-      // everything within this push/pop block
-      // will be centered, with 0,0 as the center
-      //point
-      
+      rotation = map(y,1, 100,cos(frameCount*0.02),cos(frameCount*0.01)); //wanted to use sin and cos to make the planets rotate with start values of 1 and 100 to make the rotation speed different for each row but even though it theoreticlaly makes sense I am still a little puzzled
+      rotate(rotation); //used frameCount to make it rotate continuously
+      scale(mouseY/200); //scales the elipses with mouseY position
 
       strokeWeight(3);
-      fill("#f0f321ff");
+      fill("#fdfdfd");
 
-      circle(0, 0, 100);
-      circle(-15, -10, 10);
-      circle(15, -10, 10);
-      // happiness variable will control the endpoints of the arc in my smiley face
-      let happiness;
-      happiness = map(x, 0, width, -25, 25);
-      noFill();
-      arc(0, 0, 60, 60, radians(0 - happiness), radians(180 + happiness));
+   if (mouseIsPressed == true) { //on click, adds a little saturn like planet shape made from elipses that rotates in the reverse sin and cos from earlier
+    noStroke
+fill(mouseY,mouseX,50, mouseY); //also changes fill color with mouse position
+ellipse(50, 10, 20, 5);
+      ellipse(50, 10, 10, 10);
+      rotation = map(y,100, 1,cos(frameCount*0.02),sin(frameCount*0.01));
+        }
+      
+      ellipse(50, 100, 20, 10);
+      ellipse(50, 100, 10, 20);
+   
+      //ellipse(mouseY, mouseX, 20, 10);
+
+        
       pop();
     }
   }
